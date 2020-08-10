fe6125dbe97cce02054d3c30ce46733762fa012a
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-10 21:35:43 +0700

    [Created] backend scaffold

---
+++ backend/src/main/java/com/pattaya/group1/web_service/WebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pattaya.group1.web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceApplication.class, args);
+    }
+
+}
