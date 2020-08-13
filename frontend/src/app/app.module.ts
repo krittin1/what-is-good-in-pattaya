5ec19f83440ed1392acaaddbb04037c7d4b37f25
Phatthanan Rittichai <phatthananrit@mathregit.group>
2020-08-13 13:37:15 +0700

    [Created] : form , submit component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { SubmitComponent } from './submit/submit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    SubmitComponent
   ],
   imports: [
     BrowserModule,
