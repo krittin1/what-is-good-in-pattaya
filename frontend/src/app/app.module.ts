d46503b589337b9734b03d5fd067ef069493dbf0
krittin1 <krittin.kan1@ku.th>
2020-08-13 13:40:21 +0700

    [Created]: add edit, history, login, menu and nav components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { EditComponent } from './edit/edit.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponentComponent,
+    NavComponent,
+    LoginComponent,
+    MenuComponent,
+    EditComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
