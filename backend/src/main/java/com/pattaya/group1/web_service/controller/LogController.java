e11233fe067e166732f39e30eeca2472abc2adf7
phisonkha <phison.khankhang@allianz.com>
2020-08-14 10:33:14 +0700

    [Add] ChangeLogRepository and EmployeeRepository pagination and searching schema

--- backend/src/main/java/com/pattaya/group1/web_service/controller/LogController.java
+++ backend/src/main/java/com/pattaya/group1/web_service/controller/LogController.java
@@ -1,12 +1,17 @@
 package com.pattaya.group1.web_service.controller;
 
 import com.pattaya.group1.web_service.entity.ChangeLog;
+import com.pattaya.group1.web_service.entity.Employee;
 import com.pattaya.group1.web_service.model.Log;
 import com.pattaya.group1.web_service.model.LogResponse;
+import com.pattaya.group1.web_service.model.Object;
 import com.pattaya.group1.web_service.repository.ChangeLogRepository;
+import com.pattaya.group1.web_service.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,6 +19,8 @@ public class LogController {
 
     @Autowired
     private ChangeLogRepository changeLogRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
     @PostMapping("/user")
     public String createUser(@RequestBody Log log){
         return "Post";
@@ -30,12 +37,31 @@ public class LogController {
     }
 
     @GetMapping("/logs")
-    public List<ChangeLog> getLogs(@RequestParam(required = false) String userId){
-        ChangeLog changeLog = new ChangeLog();
-        changeLog.setAction("ADD");
-        changeLog.setAdminId("phisonkha");
-        changeLog.setTimeStamp("10:00");
-        changeLogRepository.save(changeLog);
-        return changeLogRepository.findAll();
+    public LogResponse getLogs(
+            @RequestParam(required = false) String userId,
+            @RequestParam(defaultValue = "1",name = "page") int pageNumber,
+            @RequestParam(defaultValue = "10",name = "item_per_page") int itemPerPage){
+
+        List<ChangeLog> changeLogPage = changeLogRepository.findAll();
+        Employee employee = employeeRepository.findByUserId(userId);
+        List<Log> logResponseList = new ArrayList<>();
+        for(ChangeLog changeLog: changeLogPage){
+            logResponseList.add(new Log(
+                    changeLog.getMessage(),
+                    changeLog.getAdminId(),
+                    changeLog.getAdminId(),
+                    new Object(
+                            changeLog.getAction(),
+                            employee.getUserId(),
+                            changeLog.getLogId(),
+                            employee.getFirstName(),
+                            employee.getLastName(),
+                            employee.getIdentityCardNo(),
+                            employee.getPostCode()
+                            )
+            ));
+        }
+        LogResponse logResponse = new LogResponse(changeLogPage.size(),pageNumber,itemPerPage,logResponseList);
+        return logResponse;
     }
 }
