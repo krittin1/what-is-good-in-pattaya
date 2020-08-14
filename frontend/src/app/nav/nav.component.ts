a4d18298f10f81e10d81999800d8b93cf3881ec5
thanawadeetithada <thanawadeetitha@gmail.com>
2020-08-14 16:03:27 +0700

    [Created]: add complete navbar components

--- frontend/src/app/nav/nav.component.ts
+++ frontend/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -8,8 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
   }
+
 
 }
