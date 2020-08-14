7de48c2c51c6de1da00d59af2439b94c0156855a
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-14 15:32:25 +0700

    [Added] done /logs + helper endpoints

--- backend/src/main/java/com/pattaya/group1/web_service/controller/LogController.java
+++ backend/src/main/java/com/pattaya/group1/web_service/controller/LogController.java
@@ -9,59 +9,105 @@ import com.pattaya.group1.web_service.repository.ChangeLogRepository;
 import com.pattaya.group1.web_service.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
+@RequestMapping("/api/v1")
 public class LogController {
 
+    private final Logger logger = Logger.getLogger(LogController.class.getName());
     @Autowired
     private ChangeLogRepository changeLogRepository;
     @Autowired
     private EmployeeRepository employeeRepository;
+
+
     @PostMapping("/user")
-    public String createUser(@RequestBody Log log){
+    public String createUser(@RequestBody Log log) {
         return "Post";
     }
 
     @PutMapping("/user")
-    public String updateUser(@RequestBody Log log){
+    public String updateUser(@RequestBody Log log) {
         return "Put";
     }
 
     @DeleteMapping("/user/{id}")
-    public String deleteUser(@RequestParam String id){
+    public String deleteUser(@RequestParam String id) {
         return "Delete";
     }
+
 
     @GetMapping("/logs")
     public LogResponse getLogs(
             @RequestParam(required = false) String userId,
-            @RequestParam(defaultValue = "1",name = "page") int pageNumber,
-            @RequestParam(defaultValue = "10",name = "item_per_page") int itemPerPage){
+            @RequestParam(defaultValue = "0", name = "page") int pageNumber,
+            @RequestParam(defaultValue = "10", name = "item_per_page") int itemPerPage) {
 
-        List<ChangeLog> changeLogPage = changeLogRepository.findAll();
-        Employee employee = employeeRepository.findByUserId(userId);
+        Page<ChangeLog> changeLogPage;
+        if (userId != null) {
+            changeLogPage = changeLogRepository.findByUserId(PageRequest.of(pageNumber, itemPerPage), userId);
+
+
+        } else {
+
+            changeLogPage = changeLogRepository.findAll(PageRequest.of(pageNumber, itemPerPage));
+
+        }
         List<Log> logResponseList = new ArrayList<>();
-        for(ChangeLog changeLog: changeLogPage){
+        for (ChangeLog changeLog : changeLogPage) {
+            Employee userEmployee = employeeRepository.findByUserId(changeLog.getUserId());
             logResponseList.add(new Log(
                     changeLog.getMessage(),
                     changeLog.getAdminId(),
-                    changeLog.getAdminId(),
                     new Object(
                             changeLog.getAction(),
-                            employee.getUserId(),
-                            changeLog.getLogId(),
-                            employee.getInformation().getFirstName(),
-                            employee.getInformation().getLastName(),
-                            employee.getInformation().getIdentityCardNo(),
-                            employee.getInformation().getPostCode()
-                            )
+                            userEmployee.getUserId(),
+                            userEmployee.getInformation().getFirstName(),
+                            userEmployee.getInformation().getLastName(),
+                            userEmployee.getInformation().getIdentityCardNo(),
+                            userEmployee.getInformation().getAddress().getPostcode()
+                    ),
+                    changeLog.getTimestamp()
             ));
         }
-        LogResponse logResponse = new LogResponse(changeLogPage.size(),pageNumber,itemPerPage,logResponseList);
-        return logResponse;
+        return new LogResponse(changeLogPage.getNumberOfElements(), pageNumber, itemPerPage, logResponseList);
+
     }
+
+    // Helper endpoints for debugging only
+    @GetMapping({"/user/{id}", "/users"})
+    public List<Employee> getEmployee(@PathVariable(required = false) String id) {
+        if (id == null) {
+            return employeeRepository.findAll();
+        } else {
+            return Collections.singletonList(employeeRepository.findByUserId(id));
+        }
+    }
+
+
+    // Helper endpoints for debugging
+    @GetMapping({"/changelogs/{id}", "/changelogs"})
+    public String found(@PathVariable(required = false) String id, @RequestParam(required = false, defaultValue = "0") int page,
+                        @RequestParam(required = false, defaultValue = "10") int numberOfElements) {
+        Page<ChangeLog> changeLogs;
+        if (id != null)
+            changeLogs = changeLogRepository.findByUserId(PageRequest.of(page, numberOfElements), id);
+        else {
+            changeLogs = changeLogRepository.findAll(PageRequest.of(page, numberOfElements));
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (ChangeLog changeLog : changeLogs) {
+            stringBuilder.append(changeLog.toString());
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
 }
