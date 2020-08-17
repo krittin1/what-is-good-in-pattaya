d5822256de4ed2ff8623942cbdc772c902890000
Phatthanan Rittichai <phatthananrit@mathregit.group>
2020-08-17 11:04:16 +0700

    [Added] Cors

--- backend/src/main/java/com/pattaya/group1/web_service/WebServiceApplication.java
+++ backend/src/main/java/com/pattaya/group1/web_service/WebServiceApplication.java
@@ -2,6 +2,9 @@ package com.pattaya.group1.web_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebServiceApplication {
@@ -9,5 +12,16 @@ public class WebServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebServiceApplication.class, args);
     }
+
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/v1/logs").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 
 }
