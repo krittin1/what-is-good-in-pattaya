3e2c8abf9e0965fb112996178ca53671e4de445b
veeraphat-amo <veeraphat_amo@hotmail.com>
2020-08-18 17:34:50 +0700

    [Edited] Validate form

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
- docker-compose up -d
- docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
- docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/changeLog.csv"
+ winpty docker-compose up -d
+ winpty docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
+ winpty docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/changeLog.csv"
 
