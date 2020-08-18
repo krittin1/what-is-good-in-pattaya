776ecb90dbdd08551938e392509a4f9b24fcbf44
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-18 17:20:34 +0700

    [Edited] LogController updated

--- backend/src/main/java/com/pattaya/group1/web_service/repository/ChangeLogRepository.java
+++ backend/src/main/java/com/pattaya/group1/web_service/repository/ChangeLogRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ChangeLogRepository extends MongoRepository<ChangeLog, String> {
-    Page<ChangeLog> findAll(Pageable pageable);
-    Page<ChangeLog> findByUserId(Pageable pageable, String userId);
+    Page<ChangeLog> findAllByOrderByIdDesc(Pageable pageable);
+    Page<ChangeLog> findByUserIdOrderByIdDesc(Pageable pageable, String userId);
     Page<ChangeLog> findByAdminId(Pageable pageable, String AdminId);
 
 }
