2aee34acca0cb3a6377d98ac27bc4f6e0c4d82e0
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-18 15:34:11 +0700

    [Created] shell scripts for dev

---
+++ start.locol.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+ docker-compose up -d
+ docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
+ docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/changeLog.csv"
+
