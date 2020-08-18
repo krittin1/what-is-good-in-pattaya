a550f4fa93e89b0bd8bbb1709fbefebbc2e218b0
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-18 14:52:07 +0700

    [Added] /bin/bash

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#! bash
-winpty docker-compose up -d
-winpty docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
-winpty docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/changeLog.csv"
+#! /bin/bash
+ docker-compose up -d
+ docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
+ docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/changeLog.csv"
 
