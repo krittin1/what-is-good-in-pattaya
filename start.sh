10c2a3a154e6c3ff824c16796e932d1d9b66b070
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-18 15:33:30 +0700

    [Added] shell scripts for production

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
- docker-compose up -d
- docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
- docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/changeLog.csv"
+mongoimport  -d pattayaDB  --type csv --columnsHaveTypes --headerline --file postman/employee.csv
+mongoimport  -d pattayaDB  --type csv --columnsHaveTypes --headerline --file postman/changeLog.csv
 
