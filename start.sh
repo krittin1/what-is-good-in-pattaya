c329f56b901b30733354225ed1cff09a3b460450
Jiraporn Kanhasinwattana <jirapornkan@mathregit.group>
2020-08-18 11:41:12 +0700

    [Edited]: Edit code

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #! bash
-docker-compose up -d
-docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
-docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/changeLog.csv"
+winpty docker-compose up -d
+sleep 5
+winpty docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
+winpty docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/changeLog.csv"
 
