42dd683f26c5fe87208812c5e95c59e105e6ce86
Phatthanan Rittichai <phatthananrit@mathregit.group>
2020-08-19 11:30:03 +0700

    [Added] missing message in FormGroup

--- frontend/src/app/form/form.component.ts
+++ frontend/src/app/form/form.component.ts
@@ -30,6 +30,7 @@ export class FormComponent implements OnInit {
     startDate: new FormControl(),
     phoneNumber: new FormControl(),
     address: new FormControl(),
+    message: new FormControl(),
   });
 
   constructor(private service: ConfigService) {}
