9eaa198f3025ea73e341b60ee37db4d110bbe66b
phisonkha <phison.khankhang@allianz.com>
2020-08-19 10:08:22 +0700

    [Edit] set win command in start.local.sh

--- start.local.sh
+++ start.local.sh
@@ -2,3 +2,7 @@
  docker-compose up -d
  docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
 
+# windows option
+ #winpty docker-compose up -d
+ #winpty docker-compose exec mongo bash -c "mongoimport -u root -p example -d pattayaDB --authenticationDatabase admin --type csv --columnsHaveTypes --headerline --file map/employee.csv"
+
